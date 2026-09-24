41c53d985142cbe46e8c8b177714ad507671f1e3
virtual-ly-function-ing <225994451+virtual-ly-function-ing@users.noreply.github.com>
2026-09-24 16:03:50 -0400

    basic tests (fill these out later!)

---
+++ Tests/corkboard-libTests/user-management-tests.swift
@@ -0,0 +1,16 @@
+import Testing
+@testable import corkboard_lib
+
+@Suite("Authentication & Account Management")
+struct AuthenticationAccountManagementTests {
+    @Test("Successfully creates a User Record when provided with valid credentials")
+    func createUserRecordWithValidCredentials() {
+
+    }
+
+    @Test("Unsuccessfully create a User Record when provided with invalid credentials")
+    func failToCreateUserRecordWithInvalidCredentials() {
+
+    }
+
+}
