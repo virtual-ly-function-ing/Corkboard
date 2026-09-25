b19be7d975e8487bba06fc7861bda7272701d744
virtual-ly-function-ing <225994451+virtual-ly-function-ing@users.noreply.github.com>
2026-09-25 12:32:48 -0400

    Pin dependency on Argon2 to specific commit version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/P-H-C/phc-winner-argon2.git", branch: "master"),
+        .package(url: "https://github.com/P-H-C/phc-winner-argon2.git", revision: "f57e61e19229e23c4445b85494dbf7c07de721cb"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
     ],
     targets: [
